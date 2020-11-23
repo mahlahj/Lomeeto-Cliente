26e01838c6727bcbc46fcfbe8b8ed9673dde5c7e
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-11-23 00:46:21 -0600

    Se modificó la vista de posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -11,24 +11,30 @@ export default function Posts({ getPosts }) {
 
   return (
     <div className="posts">
-      <h1>Posts</h1>
+      {getPosts.length > 0 ? (
+        <>
+          <h1>Posts</h1>
 
-      {isMovil ? (
-        <Grid columns={3}>
-          {map(getPosts, (post, index) => (
-            <Grid.Column key={index} className="post-movil">
-              <PreviewPost post={post} />
-            </Grid.Column>
-          ))}
-        </Grid>
+          {isMovil ? (
+            <Grid columns={3}>
+              {map(getPosts, (post, index) => (
+                <Grid.Column key={index} className="post-movil">
+                  <PreviewPost post={post} />
+                </Grid.Column>
+              ))}
+            </Grid>
+          ) : (
+            <Grid columns={4}>
+              {map(getPosts, (post, index) => (
+                <Grid.Column key={index} className="post-margin">
+                  <PreviewPost post={post} />
+                </Grid.Column>
+              ))}
+            </Grid>
+          )}
+        </>
       ) : (
-        <Grid columns={4}>
-          {map(getPosts, (post, index) => (
-            <Grid.Column key={index} className="post-margin">
-              <PreviewPost post={post} />
-            </Grid.Column>
-          ))}
-        </Grid>
+        <h2>No hay publicaciones aún</h2>
       )}
     </div>
   );
--- src/components/Posts/PreviewPost/PreviewPost.js
+++ src/components/Posts/PreviewPost/PreviewPost.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./PreviewPost.scss";
-import { Image } from "semantic-ui-react";
+import { Image, Icon } from "semantic-ui-react";
 import ModalPost from "../../Modal/ModalPost";
 import ModalPostMovil from "../../Modal/ModalPostMovil";
 import { useMediaQuery } from "react-responsive";
+import { COUNT_LIKES } from "../../../gql/like";
+import { useQuery } from "@apollo/client";
 
 export default function PreviewPost({ post }) {
   const isMovil = useMediaQuery({ query: "(max-width: 600px)" });
@@ -15,23 +17,39 @@ export default function PreviewPost({ post }) {
   });
 
   const [showModal, setShowModal] = useState(false);
+
+  const { data: dataCount, loading: loadingCount } = useQuery(COUNT_LIKES, {
+    variables: {
+      idPost: post.id,
+    },
+  });
+
+  if (loadingCount) return null;
+  const { countLikes } = dataCount;
+
   return (
     <>
       {isTablet && (
         <div className="preview-post_tablet" onClick={() => setShowModal(true)}>
           <Image className="preview-post_image" src={post.file} />
+          <Icon name="paw" className="active" />
+          {countLikes} {countLikes === 1 ? "Paw" : "Paws"}
         </div>
       )}
 
       {isMovil && (
         <div className="preview-post_movil" onClick={() => setShowModal(true)}>
           <Image className="preview-post_image" src={post.file} />
+          <Icon name="paw" className="active" />
+          {countLikes} {countLikes === 1 ? "Paw" : "Paws"}
         </div>
       )}
 
       {isDesktopOrLaptop && (
         <div className="preview-post" onClick={() => setShowModal(true)}>
           <Image className="preview-post_image" src={post.file} />
+          <Icon name="paw" className="active" />
+          {countLikes} {countLikes === 1 ? "Paw" : "Paws"}
         </div>
       )}
 
